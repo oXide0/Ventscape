3b196dcddccb3cbe4a17fa915915794bc97d146a
Nazarii Korchevskyi <nazar10fantom@gmail.com>
2023-08-14 11:10:20 +0200

    exporting routes to a separate file

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,12 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store.ts';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<PersistGate persistor={persistor}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
+				<App />
 			</PersistGate>
 		</Provider>
 	</React.StrictMode>
---
+++ src/routes.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Layout from './hoc/Layout';
+import RequireAuth from './hoc/RequireAuth';
+import CreatorAuth from './hoc/CreatorAuth';
+import Test from './pages/Test';
+import HomePage from './pages/HomePage/HomePage';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import EventsPage from './pages/EventsPage/EventsPage';
+import CreateEventPage from './pages/CreateEventPage/CreateEventPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import EventPage from './pages/EventPage/EventPage';
+import MyEventsPage from './pages/MyEventsPage/MyEventsPage';
+import EditEventPage from './pages/EditEventPage/EditEventPage';
+import FavoriteEventsPage from './pages/FavoriteEventsPage/FavoriteEventsPage';
+import NotificationsPage from './pages/NotificationsPage/NotificationsPage';
+
+export const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<Layout />}>
+			<Route index element={<HomePage />} />
+			<Route path='signup' element={<SignUpPage />} />
+			<Route path='login' element={<LoginPage />} />
+			<Route path='test' element={<Test />} />
+			<Route path='events' element={<EventsPage />} />
+			<Route element={<RequireAuth />}>
+				<Route path='profile' element={<ProfilePage />} />
+				<Route path='notifications' element={<NotificationsPage />} />
+				<Route path='events/favorite' element={<FavoriteEventsPage />} />
+				<Route path='events/:id' element={<EventPage />} />
+				<Route path='events/edit/:id' element={<EditEventPage />} />
+				<Route element={<CreatorAuth />}>
+					<Route path='events/create' element={<CreateEventPage />} />
+					<Route path='events/my' element={<MyEventsPage />} />
+				</Route>
+			</Route>
+			<Route path='*' element={<NotFoundPage />} />
+		</Route>
+	)
+);
