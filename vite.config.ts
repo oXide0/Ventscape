4a3e0dc1f5b53faeb2c51a25095ae23787db0bf2
nazar <nazarii.korchevskyi@student.tuke.sk>
2023-11-15 21:45:41 +0100

    setup firebase hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     resolve: {
         alias: {
             routes: '/src/routes',
@@ -16,6 +16,37 @@ export default defineConfig({
             types: '/src/types',
             store: '/src/store',
             features: '/src/features',
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id: string) {
+                    if (
+                        id.includes('@chakra-ui/react') ||
+                        id.includes('@emotion/react') ||
+                        id.includes('@emotion/styled') ||
+                        id.includes('framer-motion') ||
+                        id.includes('react-icons')
+                    ) {
+                        return '@styles';
+                    }
+
+                    if (
+                        id.includes('react-hook-form') ||
+                        id.includes('react-redux') ||
+                        id.includes('@reduxjs/toolkit') ||
+                        id.includes('react-router-dom') ||
+                        id.includes('react-dom')
+                    ) {
+                        return '@react-helpers';
+                    }
+
+                    if (id.includes('firebase')) {
+                        return '@firebase';
+                    }
+                },
+            },
         },
     },
 });
