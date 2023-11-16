0c34c23da51e1c310cc89bc91adb2b9eaced1de6
nazar <nazarii.korchevskyi@student.tuke.sk>
2023-11-16 23:36:35 +0100

    changed some build rules

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         },
     },
     build: {
+        outDir: 'build',
         rollupOptions: {
             output: {
                 manualChunks(id: string) {
