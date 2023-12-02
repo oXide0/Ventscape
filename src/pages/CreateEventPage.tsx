46cb45638853da481392a12ffe0012c4406911ce
nazar <nazarii.korchevskyi@student.tuke.sk>
2023-12-02 14:18:15 +0100

    fixed ts errors

--- src/pages/CreateEventPage.tsx
+++ src/pages/CreateEventPage.tsx
@@ -5,15 +5,15 @@ import { selectUser } from 'features/userSlice';
 import { useAppSelector } from 'hooks/redux-hooks';
 import { useSubmitting } from 'hooks/useSubmitting';
 import { createEvent, uploadEventImg } from 'services/eventActions';
-import { Event } from 'types/types';
+import { Event, EventImageValues } from 'types/types';
 
 const CreateEventPage = () => {
     const { id } = useAppSelector(selectUser);
-    const { submit } = useSubmitting(async (event: Event, eventFile: File | null) => {
+    const { submit } = useSubmitting(async (event: Event, eventImage: EventImageValues) => {
         if (id) {
             const imgId = nanoid();
             await createEvent(event, id, imgId);
-            await uploadEventImg(eventFile, imgId);
+            await uploadEventImg(eventImage.file, imgId);
         }
     });
 
