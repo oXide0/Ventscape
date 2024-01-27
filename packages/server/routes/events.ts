5087433b220f1dacbaad8cb6c896f8a294ef2fc7
Nazar <nazar10fantom@gmail.com>
2024-01-27 19:40:46 +0100

    #25 added jwt verifying for saved events

--- packages/server/routes/events.ts
+++ packages/server/routes/events.ts
@@ -21,8 +21,8 @@ router.route('/').post(verifyJwt, createEvent);
 router.route('/:id').put(verifyJwt, updateEvent);
 router.route('/:id').delete(verifyJwt, deleteEvent);
 
-router.route('/save/:userId/:eventId').post(saveEventForUser);
-router.route('/saved/:userId').get(getSavedEventsForUser);
-router.route('/unsave/:id').delete(unsaveEventForUser);
+router.route('/save/:userId/:eventId').post(verifyJwt, saveEventForUser);
+router.route('/saved/:userId').get(verifyJwt, getSavedEventsForUser);
+router.route('/unsave/:id').delete(verifyJwt, unsaveEventForUser);
 
 export default router;
