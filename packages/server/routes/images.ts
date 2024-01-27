228d6d43c400b11d72700a81753d427d05c6b9ad
Nazar <nazar10fantom@gmail.com>
2024-01-27 12:53:51 +0100

    #23 added verifying jwt token while update/delete image

--- packages/server/routes/images.ts
+++ packages/server/routes/images.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import multer from 'multer';
 import { UPLOAD_ENDPOINT } from 'shared/types';
 import { getImage, removeImage, uploadImage } from '../controllers/imagesController';
+import { verifyJwt } from '../middlewares/verifyJwt';
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const router = express.Router();
 
-router.post(UPLOAD_ENDPOINT, upload.single('file'), uploadImage);
+router.post(UPLOAD_ENDPOINT, upload.single('file'), uploadImage, verifyJwt);
 router.get('/:imageId', getImage);
-router.delete('/:imageId', removeImage);
+router.delete('/:imageId', removeImage, verifyJwt);
 
 export default router;
