5d584bdc5653be41204d7e8199b76dde9738b43c
Nazar <nazar10fantom@gmail.com>
2024-01-28 11:41:53 +0100

    revert changes

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -7,7 +7,7 @@ import router from './routes/router';
 config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const corsOptions = {
     origin: process.env.CLIENT_URL,
     credentials: true,
