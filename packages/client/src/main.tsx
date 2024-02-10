fa71b5f57e322efdabae99cd2a7dc6b15c565afe
Nazar <nazar10fantom@gmail.com>
2024-02-10 13:09:00 +0100

    setup lerna

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,17 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'store/store';
 import App from './App';
 import theme from './theme';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement!).render(
+    <StrictMode>
         <Provider store={store}>
             <ChakraProvider theme={theme}>
                 <App />
             </ChakraProvider>
         </Provider>
-    </React.StrictMode>
+    </StrictMode>
 );
