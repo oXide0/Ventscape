8d51ef1991565dcc61245c21592b78fcb27df28c
Nazar <nazar10fantom@gmail.com>
2024-02-18 18:28:35 +0100

    removed some packages

--- packages/client/src/pages/StartPage.tsx
+++ packages/client/src/pages/StartPage.tsx
@@ -1,21 +1,8 @@
 import { Heading } from '@chakra-ui/react';
-import {
-    CategoryScale,
-    Chart as ChartJS,
-    Filler,
-    LineElement,
-    LinearScale,
-    PointElement,
-    Title
-} from 'chart.js';
 import PageLayout from 'components/ui/PageLayout';
-import faker from 'faker';
 import { selectUser } from 'features/userSlice';
 import { useAppSelector } from 'hooks/redux-hooks';
-import { Line } from 'react-chartjs-2';
 import { getGreeting } from 'utils/helpers';
-
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Filler);
 
 const StartPage = () => {
     const { name } = useAppSelector(selectUser);
@@ -24,36 +11,8 @@ const StartPage = () => {
     return (
         <PageLayout>
             <Heading>{greeting}</Heading>
-            <Line data={lineData} options={options} style={{ marginTop: 10 }} />
         </PageLayout>
     );
 };
 
 export default StartPage;
-
-const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top' as const
-        },
-        title: {
-            display: true,
-            text: 'Your activity'
-        }
-    }
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const lineData = {
-    labels,
-    datasets: [
-        {
-            fill: true,
-            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-            borderColor: '#6B72FF',
-            backgroundColor: 'rgba(107, 114, 255, 0.5)'
-        }
-    ]
-};
